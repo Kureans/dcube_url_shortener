138b5a2d995efb498d1432af55249f17dc02ea2c
Kureans <leonardtengys@gmail.com>
2023-09-20 19:30:02 +0800

    Add basic markup of signup and login forms

--- client/src/components/LoginComponent.jsx
+++ client/src/components/LoginComponent.jsx
@@ -1,7 +1,27 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/esm/Container';
+
 function Login() {
-    return (
+  return (
+    <Container className='mt-5'>
         <h1>Login</h1>
-    )
+        <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text" placeholder="Enter username" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+            Submit
+        </Button>
+        </Form>
+    </Container>
+  );
 }
 
 export default Login;
--- client/src/components/SignupComponent.jsx
+++ client/src/components/SignupComponent.jsx
@@ -1,7 +1,34 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/esm/Container';
+
 function Signup() {
-    return (
+  return (
+    <Container className='mt-5'>
         <h1>Sign Up</h1>
-    );
+        <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text" placeholder="Enter username" />
+            <Form.Text className="text-muted">
+            With an account, you can view all URLs that you've previously shortened.
+            </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+            Submit
+        </Button>
+        </Form>
+    </Container>
+  );
 }
 
 export default Signup;
